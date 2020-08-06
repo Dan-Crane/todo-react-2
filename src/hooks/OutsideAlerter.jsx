d9c029db53362510e9528ad6d31a57a6265df3fb
dani <gre4kaDISCOUNT@gmail.com>
2020-08-06 16:09:13 +0900

    добавлен хук OutsideAlerter

--- src/hooks/OutsideAlerter.jsx
+++ src/hooks/OutsideAlerter.jsx
@@ -1,21 +1,28 @@
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const useOutsideAlerter = (initialValue) => {
-	const ref = useRef(null)
-	const [visible, setVisible] = useState(initialValue)
+export const useOutsideAlerter = initialValue => {
+	const ref = useRef(null);
+	const [visible, setVisible] = useState(initialValue);
 
-	const handleClickOutside = (event) => {
-		if (ref.current && !ref.current.contains(event.target)) setVisible(false)
-	}
+	const handleClickOutside = event => {
+		if (ref.current && !ref.current.contains(event.target)) {
+			event.stopPropagation();
+			setVisible(false);
+		}
+	};
+
+	const handleKeyPress = event => {
+		if (event.key === "Escape") setVisible(false);
+	};
 
 	useEffect(() => {
-		document.addEventListener('click', handleClickOutside, true)
+		document.addEventListener("click", handleClickOutside, true);
+		document.addEventListener("keydown", handleKeyPress, true);
 		return () => {
-			document.removeEventListener('click', handleClickOutside, true)
+			document.removeEventListener("click", handleClickOutside, true);
+			document.removeEventListener("keydown", handleKeyPress, true);
+		};
+	}, [ref]);
 
-		}
-	}, [ref])
-
-
-	return {visible, setVisible, ref}
-}
+	return { visible, setVisible, ref };
+};
