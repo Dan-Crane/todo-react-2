c8dc60bdce4c08801bfe91cfc55783c57e378a3d
dani <gre4kaDISCOUNT@gmail.com>
2020-09-20 08:45:57 +0900

    доработана анимация BodyTitile.jsx

--- src/components/Body/BodyTitile/BodyTitile.jsx
+++ src/components/Body/BodyTitile/BodyTitile.jsx
@@ -1,53 +1,64 @@
 import React, {useState} from "react";
 
 import './BodyTitile.scss'
-import {CSSTransition, TransitionGroup, SwitchTransition} from "react-transition-group";
+import {CSSTransition, SwitchTransition} from "react-transition-group";
 
 import {Input} from "../../InputComponent/Input";
 
 export const TitleBody = ({list, onUpdate}) => {
-	const [editMode, setEditMode] = useState(false)
+	const [editMode, setEditMode] = useState(true)
 
 	return (
 		<div className='body__title title-wrap'>
 
-				{/*<CSSTransition*/}
-				{/*	in={editMode}*/}
-				{/*	timeout={3000}*/}
-				{/*	classNames={{*/}
-				{/*		enterActive: 'title-wrap__edit-title--show',*/}
-				{/*		exitActive: 'title-wrap__edit-title--hide'*/}
-				{/*	}}*/}
-				{/*	mountOnEnter*/}
-				{/*	unmountOnExit>*/}
-				{/*	<div className='title-wrap__edit-title'>*/}
-				{/*		hi*/}
-				{/*	</div>*/}
-				{/*</CSSTransition>*/}
+			{/*<CSSTransition*/}
+			{/*	in={editMode}*/}
+			{/*	timeout={3000}*/}
+			{/*	classNames={{*/}
+			{/*		enterActive: 'title-wrap__edit-title--show',*/}
+			{/*		exitActive: 'title-wrap__edit-title--hide'*/}
+			{/*	}}*/}
+			{/*	mountOnEnter*/}
+			{/*	unmountOnExit>*/}
+			{/*	<div className='title-wrap__edit-title'>*/}
+			{/*		hi*/}
+			{/*	</div>*/}
+			{/*</CSSTransition>*/}
 
-			<SwitchTransition>
+			<SwitchTransition mode='out-in'>
 				<CSSTransition
 					key={editMode}
 					in={editMode}
 					timeout={300}
-					classNames='fade'>
+					classNames={'fade'}>
 					{editMode
-						? <Input placeholder={'Название списка'} text={list.name} type='text'/>
-						: <h2 style={{color: list.color ? list.color.hex : 'black'}} className='title-wrap__title'>
-							{list.name}
-						</h2>}
+						?
+						<div>
+							<Input placeholder={'Название списка'} text={list.name} type='text'
+										 // onBlur={() => setEditMode(!editMode)}
+							/>
+
+						</div>
+						:
+						<>
+							<h2 style={{color: list.color ? list.color.hex : 'black'}} className='title-wrap__title'>
+								{list.name}
+							</h2>
+							<div className='title-wrap__img-box'>
+
+								<abbr onClick={() => setEditMode(!editMode)}
+											data-icon="c"
+											className='title-wrap__edit'/>
+								<abbr data-icon="a" className='title-wrap__sort'/>
+
+							</div>
+						</>
+					}
 
 				</CSSTransition>
 			</SwitchTransition>
 
-				<div className='title-wrap__img-box'>
 
-					<abbr onClick={() => setEditMode(!editMode)}
-								data-icon="c"
-								className='title-wrap__edit'/>
-					<abbr data-icon="a" className='title-wrap__sort'/>
-
-				</div>
 		</div>
 
 	)
--- src/components/InputComponent/Input.jsx
+++ src/components/InputComponent/Input.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 import './Input.scss'
 
-export const Input = ({placeholder, text, type}) => {
+export const Input = ({placeholder, text, type, onBlur, stateBlur}) => {
 	const [input, setInput] = useState(text)
 	return (
 		<div className='input-wrap'>
@@ -12,6 +12,7 @@ export const Input = ({placeholder, text, type}) => {
 							 placeholder='&nbsp;'
 							 value={input}
 							 autoFocus
+							 onBlur={onBlur ? onBlur : null}
 							 onChange={(event => setInput(event.target.value))}/>
 				<span className="label">{placeholder}</span>
 				<span className="focus-bg"/>
