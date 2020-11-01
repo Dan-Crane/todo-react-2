c62ad37a4405bf6be43da8633c8903f95705b423
dan <gre4kaDISCOUNT@gmail.com>
2020-11-01 16:12:40 +0900

    Анимация блока AddTaskItem.jsx

--- src/components/Body/BodyContent/AddTaskItem/AddTaskItem.jsx
+++ src/components/Body/BodyContent/AddTaskItem/AddTaskItem.jsx
@@ -1,78 +1,50 @@
 import React, {useState} from "react";
+import {CSSTransition, SwitchTransition, TransitionGroup} from "react-transition-group";
 
 import './AddTaskItem.scss'
 
-import addSvg from '../../../../assets/icons/add.svg'
+import {Input} from "../../../InputComponent/Input";
 
 export const AddTaskItem = ({idList, onAddTask, onSubmit}) => {
 	const [visible, setVisible] = useState(false)
 	const [inputValue, setInputValue] = useState('')
-	const [isSending, setIsSending] = useState(false)
 
 	const toggleVisible = () => {
 		setVisible(s => !s)
 		setInputValue('')
 	}
 
-	// const addTask = (e) => {
-	// 	e.preventDefault()
-	{/*	if(!inputValue){*/
-	}
-	{/*		return alert('Введите задачу')*/
-	}
-	{/*	}*/
-	}
-	{/*	const obj = {*/
-	}
-	// 		listId: idList,
-	// 		text: inputValue,
-	// 		completed: false
-	// 	}
-	// 	setIsSending(true)
-	// 	api.addTask(idList, obj)
-	// 		.then(({data}) => {
-	// 			onAddTask(idList, data)
-	// 			toggleVisible()
-	{/*		})*/
-	}
-	// 		.catch(() => {
-	// 			alert('Ошибка при добавлении задачи')
-	// 		})
-	// 		.finally(() => {
-	// 			setIsSending(false)
-	// 		})
-	// }
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		onSubmit(inputValue)
-		setInputValue('')
-	}
-
 	return (
-		<form onSubmit={handleSubmit} className='add-task-item'>
-			{visible
-				? <div className='add-task-item__form'>
-					<input className='main-input add-task-item__input'
-								 placeholder='Текст задачи'
-								 value={inputValue}
-								 autoFocus={true}
-								 onChange={(e) => setInputValue(e.target.value)}
-								 type="text"/>
-					<button className='add-task-item__btn-add main-btn'>
-						Добавить задачу</button>
-					<button onClick={toggleVisible}
-									className='add-task-item__btn-close'>Отмена
-					</button>
-				</div>
-				: <div onClick={toggleVisible}
-							 className='add-task-item__wrapper'>
-					<abbr className='add-task-item__icon' data-icon="e"></abbr>
-					{/*<img className='add-task-item__icon'*/}
-					{/*		 src={addSvg}*/}
-					{/*		 alt="Добавить список"/>*/}
-					<span className='add-task-item__title'>Новая задача</span>
-				</div>}
+		<div className='add-task-item'>
 
-		</form>
+			<SwitchTransition mode='out-in'>
+				<CSSTransition key={visible}
+											 in={visible}
+											 timeout={300}
+											 classNames={'fade'}>
+					<>
+						{visible
+							? <div className='add-task-item__form'>
+								<div className='add-task-item__wrap-itp'>
+									<Input placeholder='Название задачи' type='text' value={inputValue} setValue={setInputValue}/>
+								</div>
+								<button className='add-task-item__btn-add main-btn'>
+									Добавить задачу
+								</button>
+								<button onClick={toggleVisible}
+												className='add-task-item__btn-close'>Отмена
+								</button>
+							</div>
+							: <div onClick={toggleVisible}
+										 className='add-task-item__wrapper'>
+								<svg className="icon-plus-1 add-task-item__icon">
+									<use xlinkHref="#icon-plus-1"/>
+								</svg>
+								<span className='add-task-item__title'>Новая задача</span>
+							</div>}
+					</>
+				</CSSTransition>
+			</SwitchTransition>
+		</div>
 	)
 }
